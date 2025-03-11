4a34c17ad59a47258a2c09e10c5d62b4276f291a
RossiHD1810 <rossimirco1810@gmail.com>
2025-03-11 15:47:21 +0100

    Parametri Fine-Tuning
    fatto testing con modello senza fine tuning e inserito i primi parametri per fine tuning

--- main.py
+++ main.py
@@ -1,40 +1,70 @@
-import os
-os.environ['OPENai_API_KEY']='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'
-from langchain_core.prompts import ChatPromptTemplate
-from langchain_community.utilities import SQLDatabase
-from langchain_core.output_parsers import StrOutputParser
-from langchain_core. runnables import RunnablePassthrough
-from langchain_openai import ChatOpenAI
-from transformers import T5ForConditionalGeneration, T5Tokenizer
+from sklearn.model_selection import train_test_split
+from datasets import Dataset, DatasetDict, load_dataset, interleave_datasets, load_from_disk
+from transformers import AutoModelForSeq2SeqLM, AutoTokenizer, GenerationConfig, TrainingArguments, Trainer
+import torch
+import time
+import evaluate
+import pandas as pd
+import numpy as np
 
-model=T5ForConditionalGeneration, T5Tokenizer
-tokenizer=T5Tokenizer.from_pretrained('t5-small')
-
-#Funzione per Generare il Testo
-def generate_text(prompt_text):
-    input_ids=tokenizer.encode(prompt_text, return_tensor='pt')
-    outputs=model.generate(input_ids, max_lenght=512)
-    return tokenizer.decode(outputs[0], skip_special_tokens=True)
-
-def get_schema(_):
-    return db.get_table_info()
-
-template = f""""
-Based on the table schema below, write a SQL query that would answer the user's question:
-{schema}
-
-Question: {question}
-SQL Query :
-"""
-
-db_uri="mysql+mysqlconnector://root:rossimirco1810@localhost:3306/chinook"
-db=SQLDatabase.from_uri(db_uri)
-""" print(db.run('SELECT * FROM Album LIMIT 5')) """
-
-result = generate_text(prompt_text)
-print(result)
+import warnings
+warnings.filterwarnings("ignore")
 
 
+#si occupa di trasformare dati testuali in un formato numerico adatto a dare in pasto alla LLM per l'addestramento
+def tokenize_function(example):
+    
+    start_prompt = "Tables:\n"
+    middle_prompt = "\n\nQuestion:\n"
+    end_prompt = "\n\nAnswer:\n"
+  
+    data_zip = zip(example['context'], example['question'])
+    prompt = [start_prompt + context + middle_prompt + question + end_prompt for context, question in data_zip]
+    example['input_ids'] = tokenizer(prompt, padding="max_length", truncation=True, return_tensors="pt").input_ids
+    example['labels'] = tokenizer(example['answer'], padding="max_length", truncation=True, return_tensors="pt").input_ids
+
+    return example
 
 
-#sql_chain.invoke({"question":"how many artists are there?"})
+model_name='t5-small'
+tokenizer=AutoTokenizer.from_pretrained(model_name)
+original_model = AutoModelForSeq2SeqLM.from_pretrained(model_name, torch_dtype=torch.bfloat16)
+original_model = original_model.to('cuda')
+
+
+#carico i due dataset
+try:
+    dataset = load_from_disk("merged_dataset")
+    print("Loaded Merged Dataset")
+except:
+    dataset_scc_train = load_dataset("b-mc2/sql-create-context", split='train[:80%]')
+    dataset_scc_test  = load_dataset("b-mc2/sql-create-context", split='train[-20%:-10%]')
+    dataset_scc_val   = load_dataset("b-mc2/sql-create-context", split='train[-10%:]')
+    dataset_ks_train  = load_dataset("knowrohit07/know_sql", split='validation[:80%]')
+    dataset_ks_test   = load_dataset("knowrohit07/know_sql", split='validation[-20%:-10%]')
+    dataset_ks_val    = load_dataset("knowrohit07/know_sql", split='validation[-10%:]')
+
+    dataset = DatasetDict({ 'train': interleave_datasets([dataset_scc_train, dataset_ks_train]),
+                            'test': interleave_datasets([dataset_scc_test, dataset_ks_test]),
+                            'validation': interleave_datasets([dataset_scc_val, dataset_ks_val])})
+
+    dataset.save_to_disk("merged_dataset")
+    print("Merged and Saved Dataset")
+
+
+try:
+    tokenized_datasets = load_from_disk("tokenized_datasets")
+    print("Loaded Tokenized Dataset")
+except:
+    tokenized_datasets = dataset.map(tokenize_function, batched=True)
+    tokenized_datasets = tokenized_datasets.remove_columns(['question', 'context', 'answer'])
+    
+    tokenized_datasets.save_to_disk("tokenized_datasets")
+    print("Tokenized and Saved Dataset")
+
+print(f"Shapes of the datasets:")
+print(f"Training: {tokenized_datasets['train'].shape}")
+print(f"Validation: {tokenized_datasets['validation'].shape}")
+print(f"Test: {tokenized_datasets['test'].shape}")
+
+print(tokenized_datasets)
