164012a72aa583fa9909bf48689654d177d6e42a
RossiHD1810 <rossimirco1810@gmail.com>
2025-03-13 12:03:17 +0100

    Finito Addestramento e primi Testing
    Addestramento Modello Finito e salvato,  inoltre effettuando i primi test il modello sa generare query corrette con strutture database non troppo complesse

--- main.py
+++ main.py
@@ -26,8 +26,8 @@ def tokenize_function(example):
     return example
 
 
-print(torch.cuda.is_available())
-print(torch.version.cuda)
+#print(torch.cuda.is_available())
+#print(torch.version.cuda)
 model_name='t5-small'
 tokenizer=AutoTokenizer.from_pretrained(model_name)
 original_model = AutoModelForSeq2SeqLM.from_pretrained(model_name, torch_dtype=torch.bfloat16)
@@ -81,8 +81,51 @@ print(f"Test: {tokenized_datasets['test'].shape}")
 
 print(tokenized_datasets)
 
-#testing senza training
-index=0
+
+
+#-----Fine-Tuning-----
+try:
+    finetuned_model = AutoModelForSeq2SeqLM.from_pretrained("finetuned_model_2_epoch")
+    finetuned_model = finetuned_model.to('cuda')
+    to_train = False
+
+except:
+    to_train = True
+    finetuned_model = AutoModelForSeq2SeqLM.from_pretrained(model_name, torch_dtype=torch.bfloat16)
+    finetuned_model = finetuned_model.to('cuda')
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+if to_train:
+    output_dir=f'./sql-training-1741776900'
+    training_args = TrainingArguments(
+        output_dir=output_dir,
+        learning_rate=5e-3,
+        num_train_epochs=1,
+        per_device_train_batch_size=16,     # batch size per device during training
+        per_device_eval_batch_size=16,      # batch size for evaluation
+        weight_decay=0.01,
+        logging_steps=50,
+        evaluation_strategy='steps',        # evaluation strategy to adopt during training
+        eval_steps=500,                     # number of steps between evaluation
+    )
+
+    trainer = Trainer(
+        model=finetuned_model,
+        args=training_args,
+        train_dataset=tokenized_datasets['train'],
+        eval_dataset=tokenized_datasets['validation'],
+    )
+    
+    trainer.train(resume_from_checkpoint=True)
+    
+    finetuned_model.save_pretrained("finetuned_model_1_epoch")
+
+finetuned_model = AutoModelForSeq2SeqLM.from_pretrained("finetuned_model_1_epoch")
+finetuned_model = finetuned_model.to('cuda')
+
+#index indica l'indice del dataset da testare
+index=8
+#testing senza training (Zero Shot Inferencing)
 question=dataset['test'][index]['question']
 context=dataset['test'][index]['context']
 answer=dataset['test'][index]['answer']
@@ -103,37 +146,90 @@ output=tokenizer.decode(
 )
 dash_line='-'.join('' for x in range(100))
 print(dash_line)
+print(f'#####Zero_Shot#####')
 print(f'INPUT PROMPT:\n{prompt}')
 print(dash_line)
 print(f'BASELINE HUMAN ANSWER: \n{answer}')
 print(dash_line)
 print(f'MODEL GENERATION - ZERO SHOT:\n{output}')
 
-#-----Fine-Tuning-----
-finetuned_model=AutoModelForSeq2SeqLM.from_pretrained(model_name,torch_dtype=torch.bfloat16)
-finetuned_model=finetuned_model.to('cuda')
-tokenizer=AutoTokenizer.from_pretrained(model_name)
+#Risposta con modello finetuned
+question = dataset['test'][index]['question']
+context = dataset['test'][index]['context']
+answer = dataset['test'][index]['answer']
 
-output_dir=f'./sql-training-1741776900'
-training_args=TrainingArguments(
-    output_dir=output_dir,
-    #resume_from_checkpoint="C:\Users\rossi\OneDrive\Documents\Universita\Tirocinio Intergea\Demo_LangChain\sql-training-1741776900\checkpoint-5500",
-    learning_rate=5e-3,
-    num_train_epochs=1,
-    per_device_train_batch_size=16,
-    per_device_eval_batch_size=16,
-    weight_decay=0.01,
-    logging_steps=50,
-    evaluation_strategy='steps',
-    eval_steps=500,
-)
-trainer=Trainer(
-    model=finetuned_model,
-    args=training_args,
-    train_dataset=tokenized_datasets['train'],
-    eval_dataset=tokenized_datasets['validation'],
+prompt = f"""Tables:
+{context}
+
+Question:
+{question}
+
+Answer:
+"""
+
+inputs = tokenizer(prompt, return_tensors='pt')
+inputs = inputs.to('cuda')
+
+output = tokenizer.decode(
+    finetuned_model.generate(
+        inputs["input_ids"], 
+        max_new_tokens=200,
+    )[0], 
+    skip_special_tokens=True
 )
 
-trainer.train(resume_from_checkpoint=True)
+dash_line = '-'.join('' for x in range(100))
+print(dash_line)
+print(f'#####Fine_Tuned#####')
+print(f'INPUT PROMPT:\n{prompt}')
+print(dash_line)
+print(f'BASELINE HUMAN ANSWER:\n{answer}\n')
+print(dash_line)
+print(f'MODEL GENERATION - Fine_Tuned:\n{output}')
+
+
+#-----Valutazione Performance-----
+question=dataset['test'][50:75]['question']
+context=dataset['test'][50:75]['context']
+human_baseline_answers=dataset['test'][50:75]['answer']
+
+original_model_answers=[]
+finetuned_model_answers=[]
+
+for idx, question in enumerate(question):
+    prompt=f"""Tables:
+    {context[idx]}
+    Question:{question}
+    Answer:
+    """
+input_ids=tokenizer(prompt,return_tensors='pt').input_ids
+input_ids=input_ids.to('cuda')
+
+human_baseline_text_output=human_baseline_answers[idx]
+
+original_model_outputs = original_model.generate(input_ids=input_ids, generation_config=GenerationConfig(max_new_tokens=300))
+original_model_text_output = tokenizer.decode(original_model_outputs[0], skip_special_tokens=True)
+original_model_answers.append(original_model_text_output)
+
+finetuned_model_outputs = finetuned_model.generate(input_ids=input_ids, generation_config=GenerationConfig(max_new_tokens=300))
+finetuned_model_text_output = tokenizer.decode(finetuned_model_outputs[0], skip_special_tokens=True)
+finetuned_model_answers.append(finetuned_model_text_output)
+
+zipped_summaries=list(zip(human_baseline_answers,original_model_answers,finetuned_model_answers))
+df=pd.DataFrame(zipped_summaries,columns=['human_baseline_answers','original_model_answers','finetuned_model_answers'])
+
+rouge=evaluate.load('rouge')
+
+original_model_results=rouge.compute(predictions=original_model_answers,references=human_baseline_answers[0:len(original_model_answers)],
+use_aggregator=True,use_stemmer=True)
+print('Original Model:')
+print(original_model_results)
+
+finetuned_model_results=rouge.compute(predictions=finetuned_model_answers,references=human_baseline_answers[0:len(finetuned_model_answers)],
+use_aggregator=True,use_stemmer=True)
+print('Fine-Tuned Model:')
+print(finetuned_model_results)
+
+
 
 
